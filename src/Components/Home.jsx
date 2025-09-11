086d3f5d05374815285cdadd65d30f59fc76c751
Vivek Singh <vviveksingh8874@gmail.com>
2025-09-11 16:24:49 +0530

    first file added

---
+++ src/Components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='container p-4'>
+        <div className='text-center'>
+      <img src="media/images/homeHero.png" alt="hero-image" className="mb-5"/>
+      <h1 className="mt-5">Invest in everything</h1>
+        <p>
+          Online platform to invest in stocks, derivatives, mutual funds, and
+          more
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Home

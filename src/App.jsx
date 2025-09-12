956e24c2fae144dd44a4cba2a2a245d31e4fa947
Vivek Singh <vviveksingh8874@gmail.com>
2025-09-12 17:57:53 +0530

    changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import Home from "./Components/Home";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Signup from "./Components/Signup";
 import Login from "./Components/Login";
+import Navbar from "./Components/Navbar";
+import Pricing from "./Components/Pricing";
+import About from "./Components/About";
+
 
 function App() {
 
   return (
     <>
     <BrowserRouter>
+    <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/signup" element={<Signup/>}/>

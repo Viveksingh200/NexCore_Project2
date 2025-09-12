2f35c6258fbe3c369fc447e7208f9bee758d0f7c
Kedare Shweta <kedareshweta9696@gmail.com>
2025-09-12 17:22:57 +0530

    Home Page Added

---
+++ src/Components/HomePage/OpenAccount.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function OpenAccount() {
+    return (
+        <div>
+            <div className="container mx-auto p-5 mb-5">
+                <div className="text-center">
+                    <h1 className="mt-5 text-3xl font-bold">Open a Zerodha account</h1>
+                    <p className="mt-2 text-gray-600">
+                        Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and
+                        F&amp;O trades.
+                    </p>
+                    <button
+                        className="mt-5 mb-5 w-1/5 mx-auto p-2 text-lg font-medium rounded-lg bg-blue-600 text-white hover:bg-blue-700 transition"
+                    >
+                        Sign up Now
+                    </button>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default OpenAccount

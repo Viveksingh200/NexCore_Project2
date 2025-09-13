d36e6b81df82b962bd4b5ca3fad26c39c2c44359
akshay kharat <Akshay Kharat>
2025-09-13 13:03:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import Home from "./Components/Home";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Signup from "./Components/Signup";
 import Login from "./Components/Login";
+import Footer from "./Components/Footer";
+
 
 function App() {
 
+  
   return (
     <>
     <BrowserRouter>
@@ -12,8 +15,11 @@ function App() {
       <Route path="/" element={<Home/>}/>
       <Route path="/signup" element={<Signup/>}/>
       <Route path="/login" element={<Login/>}/>
+      
       </Routes>
+      <Footer/>
     </BrowserRouter>
+    
     </>
   )
 }
